e1a1041f487e0b2436ead06a882ef36b06154122
MMinusOne <michael.business.lb@proton.me>
2025-02-13 22:01:01 +0200

    first section layout change

--- components/Section-1/StatsSection.tsx
+++ components/Section-1/StatsSection.tsx
@@ -3,8 +3,8 @@ import Card from "./Card";
 
 export const StatsSection: React.FC = () => {
   return (
-    <div className="flex max-md:flex-col justify-center items-center gap-5">
-      <div className="max-md:ml-0 w-[24%] max-md:w-full">
+    <div className="max-md:hidden flex justify-center items-center gap-5">
+      <div className="max-md:ml-0 max-md:w-full">
         <Card title="Time" footer="Lebanon, Beirut">
           {new Date()
             .toLocaleTimeString("en-US", {
@@ -15,12 +15,12 @@ export const StatsSection: React.FC = () => {
             .replaceAll(" ", "")}
         </Card>
       </div>
-      <div className="ml-5 max-md:ml-0 w-[24%] max-md:w-full">
+      <div className="ml-5 max-md:ml-0 max-md:w-full">
         <Card title="Commits This Month" footer="Commits">
           <CountUp end={300} duration={4} />
         </Card>
       </div>
-      <div className="ml-5 max-md:ml-0 w-[24%] max-md:w-full">
+      <div className="ml-5 max-md:ml-0 max-md:w-full">
         <Card title="Typing Speed" footer="top 3% globally">
           <CountUp end={110} duration={4} suffix="WPM" />
         </Card>
